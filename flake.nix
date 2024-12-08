b9ad3f6341a6ccd136ad50d8b18579ec3e15545f
Nicholas Hassan <nick@hassan.host>
2024-12-08 17:00:47 +1030

    working mkMiniDevSHell function

--- flake.nix
+++ flake.nix
@@ -1,56 +1,68 @@
 {
-  description = "A flake for building dev shells";
+  description = "A flake for building development shells";
 
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     mk-minimal-shell.url = "github:n-hass/mkminimalshell";
   };
 
   outputs =
-    { self, mk-minimal-shell }:
     {
-      overlay =
-        final: prev:
-        let
-          baseMkMinimalShell = mk-minimal-shell.overlay final prev;
-        in
-        {
-          mkMiniDevShell = args:
-            let
-              shellHookOption = args.returnToUserShell or false;
-              returnToUserShellHook = let 
-                execUserShell = ''
-                  TARGET_SHELL=$(${final.pkgs.coreutils}/bin/pinky -l $USER | ${final.pkgs.gawk}/bin/awk '/Shell:/ {print $NF}')
-                  exec $TARGET_SHELL
-                '';
-              in {
-                "" = "";
-                "0" = "";
-                "1" = ''
-                  if [ "$DIRENV_IN_ENVRC" = "1" ]; then
-                    # do nothing, direnv will load a new shell
-                    :
-                  elif [ -n "$IN_NIX_SHELL" ]; then
-                    ${execUserShell}
-                  else
-                    echo "Unknown environment loader - use direnv or 'nix develop'"
-                  fi
-                '';
-                "force" = execUserShell;
-              }.${toString shellHookOption};
+      self,
+      nixpkgs,
+      flake-utils,
+      mk-minimal-shell,
+    }:
+    {
+      mkMiniDevShell = args: (flake-utils.lib.eachDefaultSystem (system: {
+        devShells =
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ mk-minimal-shell.overlay ];
+            };
+            lib = pkgs.lib;
+
+            shellHookOption = args.returnToUserShell or false;
+            returnToUserShellHook = let 
+              execUserShell = ''
+                TARGET_SHELL=$(${pkgs.coreutils}/bin/pinky -l $USER | ${pkgs.gawk}/bin/awk '/Shell:/ {print $NF}')
+                exec $TARGET_SHELL
+              '';
+            in {
+              "" = "";
+              "0" = "";
+              "1" = ''
+                if [ "$DIRENV_IN_ENVRC" = "1" ]; then
+                  # do nothing, direnv will load a new shell
+                  :
+                elif [ -n "$IN_NIX_SHELL" ]; then
+                  ${execUserShell}
+                else
+                  echo "Unknown environment loader - use direnv or 'nix develop'"
+                fi
+              '';
+              "force" = execUserShell;
+            }.${toString shellHookOption};
 
 
-              generatedArgs = {
-                extraUnsetEnv = [
-                  "returnToUserShell"
-                ];
-                
-                shellHook = ''
-                  ${args.shellHook or ""}
-                  ${returnToUserShellHook}
-                '';
-              };
-            in
-            baseMkMinimalShell.mkMinimalShell (final.lib.recursiveUpdate args generatedArgs);
-        };
+            generatedArgs = {
+              extraUnsetEnv = [
+                "returnToUserShell"
+              ];
+              
+              shellHook = ''
+                ${args.shellHook or ""}
+                ${returnToUserShellHook}
+              '';
+            };
+          in
+            {
+            default = pkgs.mkMinimalShell (lib.recursiveUpdate args generatedArgs);
+            };
+          }));
+      pkgs = nixpkgs;
+      lib = nixpkgs.lib;
     };
 }
